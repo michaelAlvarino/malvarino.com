c63b42008c331c210c5da9210937a415412fc224
michael alvarino <michael.alvarino@gmail.com>
2015-11-14 11:02:50 -0500

    add favicon

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,8 @@ import browserify from 'browserify';
 import source from 'vinyl-source-stream';
 import babelify from 'babelify';
 import sass from 'gulp-sass';
+import image from 'gulp-image';
+import * as copy from 'gulp-copy';
 
 const config = {
 	port: 3000,
@@ -13,9 +15,23 @@ const config = {
 		dist: './dist',
 		mainJs: './src/main.js',
 		sassSrc: './src/**/*.scss',
-		js: './src/**/*.js'
+		js: './src/**/*.js',
+		img: './src/img/*',
+		ico: './src/img/*.ico'
+
 	}
 };
+
+gulp.task('ico', () => {
+	return gulp.src(config.paths.ico)
+	.pipe($.copy(config.paths.dist));
+});
+
+gulp.task('images', () => {
+	gulp.src(config.paths.img)
+		.pipe(image())
+		.pipe(gulp.dest(config.paths.dist));
+});
 
 gulp.task('js', () => {
 	browserify(config.paths.mainJs)
@@ -41,6 +57,7 @@ gulp.task('watch', () => {
 	gulp.watch(config.paths.sassSrc, ['sass']);
 	gulp.watch(config.paths.html, ['html']);
 	gulp.watch(config.paths.js, ['js']);
+	gulp.watch(config.paths.img, ['images'])
 });
 
-gulp.task('default',['js', 'html', 'sass', 'watch']);
+gulp.task('default',['js', 'html', 'sass', 'images', 'watch']);
