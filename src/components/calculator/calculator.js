afe2a540f4613b92abf849a4235ce1ce7bfc2b6f
michael alvarino <michael.alvarino@gmail.com>
2015-11-27 21:10:55 -0500

    starting calculator

---
+++ src/components/calculator/calculator.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import {Router, Route, Link} from 'react-router'
+import Draggable from 'react-draggable'
+
+export default class MaNavbar extends Component{
+	render(){
+		return(
+			<div>
+				<Draggable>
+					test
+				</Draggable>
+			</div>
+		);
+	}
+}
