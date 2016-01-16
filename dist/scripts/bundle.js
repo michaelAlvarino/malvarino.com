756368d537a5145313bbb424980c890d020251fc
michael alvarino <michael.alvarino@gmail.com>
2016-01-16 15:27:20 -0500

    Finalize the styling

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -25653,21 +25653,21 @@ Calculator.propTypes = { solution: _react.PropTypes.number };
 module.exports = exports['default'];
 
 },{"../../actions/calcActions":218,"../../store/reduxStore":240,"../home/homeButton":224,"./calcInput":219,"./calculatorDescription":221,"./enumerator":222,"react":205,"react-router":45}],221:[function(require,module,exports){
-'use strict';
+"use strict";
 
-Object.defineProperty(exports, '__esModule', {
+Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var _react = require('react');
 
@@ -25679,19 +25679,28 @@ var CalculatorDescription = (function (_Component) {
   function CalculatorDescription() {
     _classCallCheck(this, CalculatorDescription);
 
-    _get(Object.getPrototypeOf(CalculatorDescription.prototype), 'constructor', this).apply(this, arguments);
+    _get(Object.getPrototypeOf(CalculatorDescription.prototype), "constructor", this).apply(this, arguments);
   }
 
   _createClass(CalculatorDescription, [{
-    key: 'render',
+    key: "render",
     value: function render() {
-      return _react2['default'].createElement(
-        'div',
-        null,
-        _react2['default'].createElement(
-          'p',
+      return _react2["default"].createElement(
+        "div",
+        { className: "CalculatorDescription" },
+        _react2["default"].createElement(
+          "p",
           null,
-          'Hey there and welcome to my calculator.'
+          "Hey there and welcome to my calculator. I know it seems silly, because a calculator is so simple, but mine is a little different. You see most calculators give you a solution, mine gives you the intermediate steps that computers go through to calculate expressions. What it does is go through the ",
+          _react2["default"].createElement(
+            "a",
+            { href: "https://en.wikipedia.org/wiki/Shunting-yard_algorithm" },
+            "Shutnting Yard Algorithm"
+          ),
+          " to break down an expression, then reconstruct its solution.",
+          _react2["default"].createElement("br", null),
+          _react2["default"].createElement("br", null),
+          "If you look below I also display the intermediary arrays/stacks/queues that I use."
         )
       );
     }
@@ -25700,8 +25709,8 @@ var CalculatorDescription = (function (_Component) {
   return CalculatorDescription;
 })(_react.Component);
 
-exports['default'] = CalculatorDescription;
-module.exports = exports['default'];
+exports["default"] = CalculatorDescription;
+module.exports = exports["default"];
 
 },{"react":205}],222:[function(require,module,exports){
 'use strict';
--- src/components/calculator/calculatorDescription.js
+++ src/components/calculator/calculatorDescription.js
@@ -2,9 +2,12 @@ import React, {Component, PropTypes} from 'react'
 
 export default class CalculatorDescription extends Component{
   render(){
-    return(<div>
+    return(
+      <div className="CalculatorDescription">
         <p>
-          Hey there and welcome to my calculator.
+          {"Hey there and welcome to my calculator. I know it seems silly, because a calculator is so simple, but mine is a little different. You see most calculators give you a solution, mine gives you the intermediate steps that computers go through to calculate expressions. What it does is go through the "}<a href="https://en.wikipedia.org/wiki/Shunting-yard_algorithm">{"Shutnting Yard Algorithm"}</a>{" to break down an expression, then reconstruct its solution."}
+        <br/><br/>
+          {"If you look below I also display the intermediary arrays/stacks/queues that I use."}
         </p>
       </div>
     );
