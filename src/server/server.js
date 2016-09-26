a79b7286b0b7e2cdf09b99695640f1145a872810
michael alvarino <michael.alvarino@gmail.com>
2016-09-25 21:58:26 -0400

    server change

--- src/server/server.js
+++ src/server/server.js
@@ -3,14 +3,14 @@ var path = require('path');
 
 var app = express();
 
-app.use(express.static('./dist/public'));
+app.use(express.static('../public'));
 
 app.get('/download/resume', (request, response) =>{
-	response.download('./dist/public/assets' + '/resume.pdf');
+	response.download('../public/assets' + '/resume.pdf');
 })
 
 app.get('*', function (request, response){
-	response.sendFile(path.resolve('./dist/public/index.html'));
+	response.sendFile(path.resolve('../public/index.html'));
 })
 
 // serve to web
